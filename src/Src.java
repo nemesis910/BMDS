e78aff321ac464068545c0d83e69ba1d494d35f5
LizTheElephant <elisabeth.stockinger@live.at>
2014-10-04 14:54:56 +0200

    updated

--- src/Src.java
+++ src/Src.java
@@ -1,55 +1,46 @@
-import java.net.*;
 import java.io.*;
+import java.net.*;
+import java.util.*;
 
-public class Src
-{  private Socket socket              = null;
-   private DataInputStream  console   = null;
-   private DataOutputStream streamOut = null;
-
-   public Src(String serverName, int serverPort)
-   {  System.out.println("Establishing connection. Please wait ...");
-      try
-      {  socket = new Socket(serverName, serverPort);
-         System.out.println("Connected: " + socket);
-         start();
-      }
-      catch(UnknownHostException uhe)
-      {  System.out.println("Host unknown: " + uhe.getMessage());
-      }
-      catch(IOException ioe)
-      {  System.out.println("Unexpected exception: " + ioe.getMessage());
-      }
-      String line = "";
-      while (!line.equals(".bye"))
-      {  try
-         {  line = console.readLine();
-            streamOut.writeUTF(line);
-            streamOut.flush();
-         }
-         catch(IOException ioe)
-         {  System.out.println("Sending error: " + ioe.getMessage());
-         }
-      }
-   }
-   public void start() throws IOException
-   {  console   = new DataInputStream(System.in);
-      streamOut = new DataOutputStream(socket.getOutputStream());
-   }
-   public void stop()
-   {  try
-      {  if (console   != null)  console.close();
-         if (streamOut != null)  streamOut.close();
-         if (socket    != null)  socket.close();
-      }
-      catch(IOException ioe)
-      {  System.out.println("Error closing ...");
-      }
-   }
-   public static void main(String args[])
-   {  Src client = null;
-      
-      client = new Src("localhost", 7007);
-   }
+public class Src{
+	static protected Set<ClientHandler> activeClients = new HashSet<ClientHandler>();
+	public static void main(String[] args){
+		System.out.println("Establishing connection. Please wait ...");
+		try{
+			Socket socket = new Socket("localhost", 7007);
+			SrcThread source = new SrcThread(socket);
+			System.out.println("Connected: " + socket);
+			source.start();
+		} catch (IOException e){
+			System.out.println(e);
+		}
+	}
 }
 
+class SrcThread extends Thread {
+	protected Socket socket;
 
+	public SrcThread(Socket socket){
+		this.socket = socket;
+	}
+	public void run(){
+		while (true){
+			DataInputStream in = new DataInputStream(System.in);
+			DataOutputStream out = null;
+			try {
+				out = new DataOutputStream(socket.getOutputStream());
+			} catch (IOException e) {
+				System.out.println("Outputstream error: " + e.getMessage());
+			}
+			
+			String line = "";
+			try {
+				line = in.readLine();
+				out.writeUTF(line);
+				out.flush();
+			} catch(IOException e) {
+				System.out.println("Sending error: " + e.getMessage());
+			}
+		}
+	}
+}

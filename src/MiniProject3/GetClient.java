c72fbcec908710754d9b00d647c2f63a11b5c651
Andrei Ursachi <ursachi.andrei1991@gmail.com>
2014-11-08 16:54:52 +0100

    GetClient and PutClient more or less done

--- src/MiniProject3/GetClient.java
+++ src/MiniProject3/GetClient.java
@@ -1,49 +1,72 @@
 package MiniProject3;
+
 import java.io.*;
 import java.net.*;
 import java.nio.ByteBuffer;
 
-
 public class GetClient {
 
 	private static int value;
-	private static DatagramSocket destinationSocket=null;
-	
-	public static void main(String[] args) throws IOException
-	{
-		if(args.length!=2)
-		{
-			System.out.println("Please input the hostname and port from where you want to get the message + the identifier of the message");
+	private static DatagramSocket destinationSocket = null;
+
+	public static void main(String[] args) throws IOException,
+			ClassNotFoundException {
+		if (args.length != 3) {
+			System.out
+					.println("Please input the hostname and port from where you want to get the message + the identifier of the message");
+
+			if (args[0] == "")
+				System.out.println("Input the host name");
+			if (args[1] == "")
+				System.out.println("Input the port");
+			if (args[2] == "")
+				System.out.println("Input the identifier value");
+
 			return;
 		}
-		
-		//create a DatagramSocket to sned
+
+		// create a DatagramSocket to sned
 		destinationSocket = new DatagramSocket();
-		
-		//socket
+
+		// socket
 		InetAddress address = InetAddress.getByName(args[0]);
 		int port = Integer.parseInt(args[1]);
-		
-		//send the request for a specific message
+
+		// send the request for a specific message
 		value = Integer.parseInt(args[2]);
 		byte[] buff = new byte[512];
-		
-		//convert the value into byte array
-		ByteBuffer b =ByteBuffer.allocate(4);
-		buff = b.putInt(value).array();
-		
-		
-		//getRequest
-		DatagramPacket packet = new DatagramPacket(buff, buff.length,address,port);
-		destinationSocket.send(packet);
-		
-		////we need a second meeting :))
-		
-		
-	}
-	
-	
 
-	
-	
+		// convert the value into byte array
+		ByteBuffer b = ByteBuffer.allocate(4);
+		buff = b.putInt(value).array();
+
+		try {
+			// getRequest
+			DatagramPacket packet = new DatagramPacket(buff, buff.length,
+					address, port);
+			destinationSocket.send(packet);
+			System.out.println("Request sent to ");
+
+			// wait for response??
+			packet = new DatagramPacket(buff, buff.length);
+			destinationSocket.receive(packet);
+			System.out.println("Message received");
+
+			Message message = deserialize(buff);
+			System.out.println(message.content);
+
+		} finally {
+			destinationSocket.close();
+		}
+	}
+
+	public static Message deserialize(byte[] bytes) throws IOException,
+			ClassNotFoundException {
+		ByteArrayInputStream b = new ByteArrayInputStream(bytes);
+		ObjectInputStream o = new ObjectInputStream(b);
+		return (Message) o.readObject();
+
+		// I hope this will work
+	}
+
 }
--- src/MiniProject3/PutClient.java
+++ src/MiniProject3/PutClient.java
@@ -1,53 +1,55 @@
 package MiniProject3;
+
 import java.net.*;
 import java.io.*;
 
 public class PutClient {
 
-	private static DatagramSocket destinationSocket=null;
+	private static DatagramSocket destinationSocket = null;
 	private static Message message;
 
 	public static void main(String[] args) throws IOException {
-		
-		if(args.length!=3)
-		{
-			System.out.println("Please input the hostname and port to which to connect + the message you want to send");
+
+		if (args.length != 3) {
+			System.out
+					.println("Please input the hostname and port to which to connect + the message you want to send");
 			return;
 		}
-				
-			//should we set the hostname to localhost??
-			InetAddress address = InetAddress.getByName(args[0]);
-			int port=Integer.parseInt(args[1]);
-			
-			//create a DatagramSocket to send it
-			destinationSocket = new DatagramSocket();
-			
-			//we create a buffer in order to send a message
-			//Message implements Serializable to be transform in an array of bytes
-			Message message=new Message(Type.PUT,args[2] );
-			byte[] buff =new byte[512]; // or max of a UDP Package			
-			buff= serialize(message);
-			
-			
-			
-			DatagramPacket packet = new DatagramPacket(buff, buff.length,address,port);
+
+		// should we set the hostname to localhost??
+		InetAddress address = InetAddress.getByName(args[0]);
+		int port = Integer.parseInt(args[1]);
+
+		// create a DatagramSocket to send it
+		destinationSocket = new DatagramSocket();
+
+		// we create a buffer in order to send a message
+		// Message implements Serializable to be transform in an array of bytes
+		Message message = new Message(Type.PUT, args[2]);
+		byte[] buff = new byte[512]; // or max of a UDP Package
+		buff = serialize(message);
+
+		try {
+
+			DatagramPacket packet = new DatagramPacket(buff, buff.length,
+					address, port);
 			destinationSocket.send(packet);
-			
+
 			System.out.println(message.content);
 			System.out.println("SENT!");
-			
-			
-		
+
+		} finally {
+			destinationSocket.close();
+
+		}
+
 	}
-	
-	
-	private static byte[] serialize(Message message) throws IOException
-	{
+
+	private static byte[] serialize(Message message) throws IOException {
 		ByteArrayOutputStream b = new ByteArrayOutputStream();
 		ObjectOutputStream o = new ObjectOutputStream(b);
 		o.writeObject(message);
 		return b.toByteArray();
 	}
-	
 
 }
